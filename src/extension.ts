4aae42aaeb38d32600a71fdf32c7187f2ce22738
David Edmiston <edmistond@gmail.com>
2017-09-13 22:55:44 -0400

    Continued development, figuring out Typescript.

--- src/extension.ts
+++ src/extension.ts
@@ -9,13 +9,71 @@ import * as _ from "lodash";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
+function getStagedFiles(files) {
+  let stagedFiles: any = _.filter(files, function(f: any) {
+    return f.index !== " ";
+  });
+
+  return stagedFiles;
+}
+
+function parseStatusInfo(statusSummary: any): string {
+  let result: string = "\n\n";
+  result += "# Add a commit message here! \n#\n";
+  result += `# Current branch: ${statusSummary.current}\n#\n`;
+
+  let stagedFiles:any = getStagedFiles(statusSummary.files);
+
+  if (stagedFiles.length === 0) {
+    return result;
+  }
+
+  result += `# Staged files:\n`;
+  _.each(stagedFiles, function(sf: any) {
+    result += `#    ${sf.path}\n`;
+  });
+
+  return result;
+}
+
 async function showCommitScreen() {
+  let gh = new gitHelper(vscode.workspace.rootPath);
+  let status: any;
+  try {
+    status = await gh.status();
+    console.log(status);
+  } catch (e) {
+    console.log(e);
+    await vscode.window.showErrorMessage("Error getting git status.");
+    return;
+  }
+
   let commitDisplay: TextDocument = await vscode.workspace.openTextDocument({
-    content: "Type a commit message!",
+    content: parseStatusInfo(status),
     language: "git-commit"
   });
 
   await vscode.window.showTextDocument(commitDisplay);
+}
+
+async function executeCommit() {
+  let activeDocument: TextDocument = vscode.window.activeTextEditor.document;
+
+  if (activeDocument.languageId !== "git-commit") {
+    return vscode.window.showErrorMessage(
+      "Current buffer is not a git commit message."
+    );
+  }
+
+  let commitMsg = activeDocument.getText();
+  // TODO: move commit message splitting to this fn and check for empty lines
+
+  let gh = new gitHelper(vscode.workspace.rootPath);
+  let status:any = await gh.status();
+  
+  if (getStagedFiles(status.files).length === 0) {
+    return vscode.window.showErrorMessage("No files are currently staged.");
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -24,7 +82,13 @@ export function activate(context: vscode.ExtensionContext) {
     showCommitScreen
   );
 
+  let execCommitCmd: Disposable = vscode.commands.registerCommand(
+    "bettercommits.executeCommit",
+    executeCommit
+  );
+
   context.subscriptions.push(showCommitCmd);
+  context.subscriptions.push(execCommitCmd);
 }
 
 // this method is called when your extension is deactivated
--- src/gitHelper.ts
+++ src/gitHelper.ts
@@ -1,13 +1,17 @@
-import * as git from "simple-git";
+import * as simpleGit from "simple-git";
+import * as _ from "lodash";
 
 export class gitHelper {
   private repositoryDirectory: string;
+  private sGit: any;
 
   constructor(workingDir: string) {
     this.repositoryDirectory = workingDir;
+    this.sGit = new simpleGit(this.repositoryDirectory);
   }
 
-  public status() {
+  status() {
+    let git = this.sGit;
     return new Promise(function(resolve, reject) {
       git.status(function(err, status) {
         if (err) return reject(err);
@@ -16,9 +20,15 @@ export class gitHelper {
     });
   }
 
-  public commit(message: string) {
-    let parsedMessage:string[] = message.split("\n");
-    return new Promise(function(resolve ,reject) {
+  commit(message: string) {
+    let git = this.sGit;
+    let parsedMessage: string[] = message.split("\n");
+    if (parsedMessage.length > 1) {
+      parsedMessage = _.filter(parsedMessage, function(m: string) {
+        return !m.startsWith("#");
+      });
+    }
+    return new Promise(function(resolve, reject) {
       git.commit(parsedMessage, function(err, result) {
         if (err) return reject(err);
         return resolve(result);
